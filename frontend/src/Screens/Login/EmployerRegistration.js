ff96e6ddebeb5ad16b6283c0aa738c4ae039f0d8
sanuanfan <sanuanfan8535@gmail.com>
2024-09-21 00:16:34 +0530

    employer registration completed

--- frontend/src/Screens/Login/EmployerRegistration.js
+++ frontend/src/Screens/Login/EmployerRegistration.js
@@ -4,10 +4,65 @@ import './EmployerRegistration.css'
 function EmployerRegistration() {
   return (
     <div className='maindiv'>
-        <div className="subdiv">
-            
+      <div className="subdiv">
+        <div className="head">
+          <div className="head-left">
+            <div className='left'>
+              <h1>
+                Create an employer account
+
+              </h1>
+            </div>
+
+          </div>
+          <div className="head-right">
+
+          </div>
         </div>
-      
+        <form action="">
+          <div className="form">
+            <h1>You haven't posted a job before, so you'll need to create an employer account.</h1>
+
+            <div className='content'>
+              <label htmlFor="">Your company's name</label>
+              <input type="text"  required/>
+            </div>
+            <div className='content'>
+              <label htmlFor="">Your company's number of employees</label>
+              <select name="no of employee" id="" required>
+                <option value="select an oprion" >Select an option</option>
+                <option value="1 to 49">1 to 49</option>
+                <option value="50 to 149">50 to 149</option>
+                <option value="150 to 249">150 to 249</option>
+                <option value="250 to 499">250 to 499</option>
+                <option value="500 to 799">500 to 799</option>
+                <option value="750 to 999">750 to 999</option>
+                <option value="1000+">1000+</option>
+
+              </select>
+            </div>
+
+            <div className='content'>
+              <label htmlFor="">Your first and last name</label>
+              <input type="text" required/>
+            </div>
+            <div className='content'>
+              <label htmlFor="">Your phone number </label>
+              <input type="text" required/>
+            </div>
+            <div className="button">
+            <div className="continue">
+              Continue
+              <i class='bx bx-right-arrow-alt' style={{fontSize:"20px"}}></i>
+            </div>
+            </div>
+            
+
+          </div>
+        </form>
+
+      </div>
+
     </div>
   )
 }
