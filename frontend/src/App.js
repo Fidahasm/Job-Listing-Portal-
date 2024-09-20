5163cb166fa9062cbe03343a0a0eaa1f8a1ff540
sanuanfan <sanuanfan8535@gmail.com>
2024-09-20 20:57:08 +0530

    set routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from "./Screens/HomePage/HomePage";
+import Login from "./Screens/Login/Login";
+import Registration from "./Screens/Login/Registration"
+import Admin from "./Screens/Admin/Admin";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route>
-          
-        </Route>
+        <Route path="/" element = {<HomePage/>}/>
+        <Route path="/login" element = {<Login/>}/>
+        <Route path="/register" element = {<Registration/>}/>
+        <Route path="/admin" element = {<Admin/>}/>
       </Routes>
     </Router>
  
